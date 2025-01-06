af45d097353d462601692034c922e73a20b92263
Rafa-Ol-Dev <111985568+Rafa-Ol-Dev@users.noreply.github.com>
2025-01-06 10:45:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,7 @@
-
-/*
-Instruções para entrega
-# 1️⃣ Desafio Classificador de nível de Herói
-
-**O Que deve ser utilizado**
-
-- Variáveis
-- Operadores
-- Laços de repetição
-- Estruturas de decisões
-
-## Objetivo
-
-Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
-
-Se XP for menor do que 1.000 = Ferro
-Se XP for entre 1.001 e 2.000 = Bronze
-Se XP for entre 2.001 e 5.000 = Prata
-Se XP for entre 5.001 e 7.000 = Ouro
-Se XP for entre 7.001 e 8.000 = Platina
-Se XP for entre 8.001 e 9.000 = Ascendente
-Se XP for entre 9.001 e 10.000= Imortal
-Se XP for maior ou igual a 10.001 = Radiante
-
-## Saída
-
-Ao final deve se exibir uma mensagem:
-"O Herói de nome **{nome}** está no nível de **{nivel}**"
-*/
-
-console.log("# Desafio Classificador de Nível de Herói");
-console.log("Qual o nome de seu herói? ");
+console.log("Desafio Classificador de Nível de Herói\n");
+console.log("Qual o nome de seu herói? \n");
 const nomeDoHeroi = "Conan";
-console.log("Quantos pontos de experiência (XP) seu herói tem? ");
+console.log("Quantos pontos de experiência (XP) seu herói tem? \n");
 const xpDoHeroi = 9000;
 
 let nivelDoHeroi;
